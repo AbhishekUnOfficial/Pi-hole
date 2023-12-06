14ed2114c207e0973b04a8536794e3ecf3032c15
Abhishek Mallick <78242724+AbhishekUnOfficial@users.noreply.github.com>
2023-12-06 16:59:51 +0530

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -21,5 +21,10 @@ fi
 # Restart Unbound
 sudo systemctl restart unbound
 
+# Enable ip forwarding
+echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
+sudo sysctl -p /etc/sysctl.d/99-tailscale.conf
+
 # Start Tailscale
 sudo tailscale up --advertise-exit-node --accept-dns=true --ssh
