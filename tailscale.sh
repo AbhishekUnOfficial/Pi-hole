c9b4dbf75dd6515b1329478fb549eaf08374a5a2
Abhishek Mallick <78242724+AbhishekUnOfficial@users.noreply.github.com>
2023-12-06 15:26:18 +0530

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Install unbound
-sudo apt install unbound -y
-
-# Move pi-hole.conf file
-mv pi-hole.conf /etc/unbound/unbound.conf.d/
-
 # Install tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
 # start tailscale
 sudo tailscale up --advertise-exit-node --accept-dns=true --ssh
+
+# Install Pi-hole
+curl -sSL https://install.pi-hole.net | bash
+
+# Install unbound
+sudo apt install unbound -y
+
+# Move pi-hole.conf file
+mv pi-hole.conf /etc/unbound/unbound.conf.d/
