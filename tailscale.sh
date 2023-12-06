b445c3683cc24cd1a68781e07ceffbb576e56853
Abhishek Mallick <78242724+AbhishekUnOfficial@users.noreply.github.com>
2023-12-06 15:37:20 +0530

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Install tailscale
+# Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
 
-# start tailscale
+# Start Tailscale
 sudo tailscale up --advertise-exit-node --accept-dns=true --ssh
 
 # Install Pi-hole
 curl -sSL https://install.pi-hole.net | bash
 
-# Install unbound
+# Install Unbound
 sudo apt install unbound -y
 
-# Move pi-hole.conf file
-mv pi-hole.conf /etc/unbound/unbound.conf.d/
+# Move pi-hole.conf file if it's in the same directory as the script
+if [ -f "pi-hole.conf" ]; then
+    sudo mv pi-hole.conf /etc/unbound/unbound.conf.d/
+else
+    echo "pi-hole.conf not found in the current directory. Please provide the correct path."
+    # If the pi-hole.conf file is in a different directory, specify the correct path in the 'mv' command.
+    # Example: sudo mv /path/to/pi-hole.conf /etc/unbound/unbound.conf.d/
+fi
 
-# Restart unbound
-sudo service unbound restart
+# Restart Unbound
+sudo systemctl restart unbound
